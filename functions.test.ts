bf88764be447ed305df27208ad765f03803252b4
Dallas Hepner <dallashepner@gmail.com>
2022-07-22 16:26:24 -0600

    Added start to script just in case

--- functions.test.ts
+++ functions.test.ts
@@ -2,7 +2,11 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // test('shuffleArray should return an array', () => {
-    //     expect(shuffleArray.isArray().toBe(true))
+    //     let array = [1, 2, 3, 4, 5]
+    //     let originalLenght = array.length
+    //     let shuffledArray = shuffleArray(array)
+    //     let shuffleLength = shuffledArray.lenght
+    //     expect(shuffleLength).toEqual(originalLenght)
     // })
 
     test('shuffleArray should return an array of same length', () => {
