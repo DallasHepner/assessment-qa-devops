f61682f505b41c0059bf07324b36280dc8df077d
Dallas Hepner <dallashepner@gmail.com>
2022-07-22 15:22:07 -0600

    Fix of endpoints

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ var rollbar = new Rollbar({
 rollbar.log('Hello world!')
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../assessment-qa-devops/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, '../assessment-qa-devops/public/index.css'))
+    res.sendFile(path.join(__dirname, './public/index.css'))
 })
 app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../assessment-qa-devops/public/index.js'))
+    res.sendFile(path.join(__dirname, './public/index.js'))
 })
 
 
