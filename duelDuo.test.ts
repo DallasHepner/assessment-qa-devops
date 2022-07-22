e162e4e612d7d5033c44f8601130e087fca1975a
Dallas Hepner <dallashepner@gmail.com>
2022-07-22 11:36:03 -0600

    Added Rollbar info

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,5 +1,6 @@
 
 import { Builder, Capabilities, By } from "selenium-webdriver"
+import { skipPartiallyEmittedExpressions } from "typescript"
 
 require('chromedriver')
 
@@ -15,6 +16,25 @@ afterAll(async () => {
 
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
+    await driver.sleep(2000)
     const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
+test('Draw button displays choices', async () => {
+    const choiceList = await driver.findElement(By.id('choices'))
+    await driver.sleep(2000)
+    const displayed = await choiceList.isDisplayed()
+    expect(displayed).toBe(true)
+})
+test('Add to duo button displays player-duo', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(2000)
+    await driver.findElement(By.id('choices'))
+    await driver.sleep(2000)
+    await driver.findElement(By.id('bot-btn')).click()
+    const duoList = await driver.findElement(By.id('player-duo'))
+    await driver.sleep(2000)
+    const displayed = await duoList.isDisplayed()
+    await driver.sleep(2000)
     expect(displayed).toBe(true)
 })
