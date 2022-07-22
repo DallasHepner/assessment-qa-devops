c9ad5f9ac227dc207caef569da4ac434271d65a3
Dallas Hepner <dallashepner@gmail.com>
2022-07-22 11:44:59 -0600

    Installed cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const cors = require('cors')
 const {bots, playerRecord} = require('./data')
 const {shuffleArray} = require('./utils')
 
 app.use(express.json())
+app.use(cors())
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
